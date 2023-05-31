9e41d11ee1cb45840da203f90f868f0fb601c710
oliviafatu <livfatu@gmail.com>
2023-06-01 09:37:16 +1200

    new commit

--- shopping-cart/shopping-cart/shopping-cart.cpp
+++ shopping-cart/shopping-cart/shopping-cart.cpp
@@ -1,25 +1,33 @@
 #include <iostream>
 using namespace std;
 
+string userInput;
+string brand;
+int price;
+
+void productDisplay() {
+    std::cout << "Items in the shopping cart:" << std::endl;
+    std::cout << "- " << userInput << "(" << brand << ")";
+}
+
 int main()
 {
-    string userInput;
-    int price;
 
-    while (true) {
+while (true) 
+    {
         std::cout << "Enter the product name (or 'exit' to finish): ";
         std::cin >> userInput;
-        std::cout << "\nEnter the product brand: ";
-        std::cin >> userInput;
-        std::cout << "\nEnter the product price: ";
+        std::cout << "Enter the product brand: ";
+        std::cin >> brand;
+        std::cout << "Enter the product price: ";
         std::cin >> price;
 
-        std::cout << "Added " << userInput << " to the shopping cart";
+        std::cout << "Added " << userInput << " to the shopping cart.\n" << std::endl;
 
         if (userInput == "exit")
         {
             false;
-            exit(0);
+            productDisplay();
         }
     }
 }
